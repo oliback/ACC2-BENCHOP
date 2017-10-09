729f59dad5638f9bb74085ecbd8a5c8bc16c2250
oliback <oliback@bithack.se>
2017-10-09 23:51:34 +0200

    Added functionality for passing arguments specifying which problems to run. This only works in Octave since argv() is not supported in Windows. This does not matter very much for this project. In windows, p1a, p1b, and p1c are run by default now.
    The possible arguments are:
    p1a
    p1b
    p1c
    p2a
    p2b
    p2c
    all
    
    all runs all problems.
    Example of how the command line arguments can be passed:
    
    octave Table.m p1a p1b p2c	(runs three problems)
    octave Table.m all	(runs all problems)

--- matlab/BENCHOP/Table.m
+++ matlab/BENCHOP/Table.m
@@ -32,111 +32,149 @@ format long
 
 warning off
 
-full_test = 0
+p1a = true;
+p1b = true;
+p1c = true;
+p2a = false;
+p2b = false;
+p2c = false;
+run_all = false;
+
+% Octave supports command line arguments with argv(), windows does not.
+if ~ispc
+    args = argv();
+    if ~isempty(args)
+        % method to search for substrings found at:
+        % https://se.mathworks.com/matlabcentral/answers/50624-testing-for-the-presence-of-a-substring-in-a-cell-array-of-strings
+        % isempty instead of contains for Octave compatability
+        p1a = any(~cellfun('isempty', strfind(args, "p1a")));
+        p1b = any(~cellfun('isempty', strfind(args, "p1b")));
+        p1c = any(~cellfun('isempty', strfind(args, "p1c")));
+        p2a = any(~cellfun('isempty', strfind(args, "p2a")));
+        p2b = any(~cellfun('isempty', strfind(args, "p2b")));
+        p2c = any(~cellfun('isempty', strfind(args, "p2c")));
+    end
+end
 
 Methods={'MC','MC-S','QMC-S','MLMC','MLMC-A',...
     'FFT','FGL','COS',...
     'FD','FD-NU','FD-AD',...
     'RBF','RBF-FD','RBF-PUM','RBF-LSML','RBF-AD','RBF-MLT'};
 
-if full_test
-    disp("running all tests")
-else
-    disp("runninq the quick tests")
-end
-
 %% Problem 1 a) I
 
-display('Problem 1 a) I');
-%If there are problems, make sure that pwd is actually the current BENCHOP root.
-rootpath=pwd;
-S=[90,100,110]; K=100; T=1.0; r=0.03; sig=0.15;
-U=[2.758443856146076 7.485087593912603 14.702019669720769];
+if p1a || run_all
+    display('Problem 1 a) I');
+    %If there are problems, make sure that pwd is actually the current BENCHOP root.
+    rootpath=pwd;
+    S=[90,100,110]; K=100; T=1.0; r=0.03; sig=0.15;
+    U=[2.758443856146076 7.485087593912603 14.702019669720769];
 
-filepathsBSeuCallUI=getfilenames('./','BSeuCallUI_*.m');
-par={S,K,T,r,sig};
-[timeBSeuCallUI,relerrBSeuCallUI] = executor(rootpath,filepathsBSeuCallUI,U,par)
+    filepathsBSeuCallUI=getfilenames('./','BSeuCallUI_*.m');
+    par={S,K,T,r,sig};
+    [timeBSeuCallUI,relerrBSeuCallUI] = executor(rootpath,filepathsBSeuCallUI,U,par)
 
-tBSeuCallUI=NaN(numel(Methods),1); rBSeuCallUI=tBSeuCallUI;
+    tBSeuCallUI=NaN(numel(Methods),1); rBSeuCallUI=tBSeuCallUI;
 
-for ii=1:numel(Methods)
-    for jj=1:numel(filepathsBSeuCallUI)
-        a=filepathsBSeuCallUI{jj}(3:3+numel(Methods{ii}));
-        if ispc
-            b=[Methods{ii},'\'];
-        else
-            b=[Methods{ii},'/'];
-        end
-        if strcmp(a,b)
-            tBSeuCallUI(ii)=timeBSeuCallUI(jj);
-            rBSeuCallUI(ii)=relerrBSeuCallUI(jj);
+    for ii=1:numel(Methods)
+        for jj=1:numel(filepathsBSeuCallUI)
+            a=filepathsBSeuCallUI{jj}(3:3+numel(Methods{ii}));
+            if ispc
+                b=[Methods{ii},'\'];
+            else
+                b=[Methods{ii},'/'];
+            end
+            if strcmp(a,b)
+                tBSeuCallUI(ii)=timeBSeuCallUI(jj);
+                rBSeuCallUI(ii)=relerrBSeuCallUI(jj);
+            end
         end
     end
+    
+    cd(rootpath);
+else
+    %This is simply here to make table generation etc simpler
+    timeBSeuCallUI = NaN(3,1,'double')
+    relerrBSeuCallUI = NaN(3,1,'double')
+    
+    tBSeuCallUI=NaN(numel(Methods),1); rBSeuCallUI=tBSeuCallUI;
 end
-
-cd(rootpath);
 
 %% Problem 1 b) I
 
-display('Problem 1 b) I');
-rootpath=pwd;
-S=[90,100,110]; K=100; T=1.0; r=0.03; sig=0.15;
-U=[10.726486710094511 4.820608184813253 1.828207584020458];
+if p1b || run_all
+    display('Problem 1 b) I');
+    rootpath=pwd;
+    S=[90,100,110]; K=100; T=1.0; r=0.03; sig=0.15;
+    U=[10.726486710094511 4.820608184813253 1.828207584020458];
 
-filepathsBSamPutUI=getfilenames('./','BSamPutUI_*.m');
-par={S,K,T,r,sig};
-[timeBSamPutUI,relerrBSamPutUI] = executor(rootpath,filepathsBSamPutUI,U,par)
+    filepathsBSamPutUI=getfilenames('./','BSamPutUI_*.m');
+    par={S,K,T,r,sig};
+    [timeBSamPutUI,relerrBSamPutUI] = executor(rootpath,filepathsBSamPutUI,U,par)
 
-tBSamPutUI=NaN(numel(Methods),1); rBSamPutUI=NaN(numel(Methods),1);
-for ii=1:numel(Methods)
-    for jj=1:numel(filepathsBSamPutUI)
-        a=filepathsBSamPutUI{jj}(3:3+numel(Methods{ii}));
-        if ispc
-            b=[Methods{ii},'\'];
-        else
-            b=[Methods{ii},'/'];
-        end
-        if strcmp(a,b)
-            tBSamPutUI(ii)=timeBSamPutUI(jj);
-            rBSamPutUI(ii)=relerrBSamPutUI(jj);
+    tBSamPutUI=NaN(numel(Methods),1); rBSamPutUI=NaN(numel(Methods),1);
+    for ii=1:numel(Methods)
+        for jj=1:numel(filepathsBSamPutUI)
+            a=filepathsBSamPutUI{jj}(3:3+numel(Methods{ii}));
+            if ispc
+                b=[Methods{ii},'\'];
+            else
+                b=[Methods{ii},'/'];
+            end
+            if strcmp(a,b)
+                tBSamPutUI(ii)=timeBSamPutUI(jj);
+                rBSamPutUI(ii)=relerrBSamPutUI(jj);
+            end
         end
     end
-end
+    
+    cd(rootpath);
+else
+    timeBSamPutUI = NaN(3,1,'double')
+    relerrBSamPutUI = NaN(3,1,'double')
 
-cd(rootpath);
+    tBSamPutUI=NaN(numel(Methods),1); rBSamPutUI=NaN(numel(Methods),1);
+end
 
 %% Problem 1 c) I
 
-display('Problem 1 c) I');
-rootpath=pwd;
-S=[90,100,110]; K=100; T=1.0; r=0.03; sig=0.15; B=1.25*K;
-U=[1.822512255945242 3.294086516281595 3.221591131246868];
+if p1c || run_all
+    display('Problem 1 c) I');
+    rootpath=pwd;
+    S=[90,100,110]; K=100; T=1.0; r=0.03; sig=0.15; B=1.25*K;
+    U=[1.822512255945242 3.294086516281595 3.221591131246868];
 
-filepathsBSupoutCallI=getfilenames('./','BSupoutCallI_*.m');
-par={S,K,T,r,sig,B};
-[timeBSupoutCallI,relerrBSupoutCallI] = executor(rootpath,filepathsBSupoutCallI,U,par)
+    filepathsBSupoutCallI=getfilenames('./','BSupoutCallI_*.m');
+    par={S,K,T,r,sig,B};
+    [timeBSupoutCallI,relerrBSupoutCallI] = executor(rootpath,filepathsBSupoutCallI,U,par)
 
-tBSupoutCallI=NaN(numel(Methods),1); rBSupoutCallI=NaN(numel(Methods),1);
-for ii=1:numel(Methods)
-    for jj=1:numel(filepathsBSupoutCallI)
-        a=filepathsBSupoutCallI{jj}(3:3+numel(Methods{ii}));
-        if ispc
-            b=[Methods{ii},'\'];
-        else
-            b=[Methods{ii},'/'];
-        end
-        if strcmp(a,b)
-            tBSupoutCallI(ii)=timeBSupoutCallI(jj);
-            rBSupoutCallI(ii)=relerrBSupoutCallI(jj);
+    tBSupoutCallI=NaN(numel(Methods),1); rBSupoutCallI=NaN(numel(Methods),1);
+    for ii=1:numel(Methods)
+        for jj=1:numel(filepathsBSupoutCallI)
+            a=filepathsBSupoutCallI{jj}(3:3+numel(Methods{ii}));
+            if ispc
+                b=[Methods{ii},'\'];
+            else
+                b=[Methods{ii},'/'];
+            end
+            if strcmp(a,b)
+                tBSupoutCallI(ii)=timeBSupoutCallI(jj);
+                rBSupoutCallI(ii)=relerrBSupoutCallI(jj);
+            end
         end
     end
+    
+    cd(rootpath);
+else
+    timeBSupoutCallI = NaN(3,1,'double')
+    relerrBSupoutCallI = NaN(3,1,'double')
+    
+    tBSupoutCallI=NaN(numel(Methods),1); rBSupoutCallI=NaN(numel(Methods),1);
 end
-
-cd(rootpath);
 
 %% Problem 1 a) II
 
-if full_test
+if p2a || run_all
     display('Problem 1 a) II');
     rootpath=pwd;
     S=[97,98,99]; sig=0.01; r=0.1; T=0.25; K=100;
@@ -162,6 +200,8 @@ if full_test
             end
         end
     end
+    
+    cd(rootpath);
 else
     timeBSeuCallUII = NaN(3,1,'double')
     relerrBSeuCallUII = NaN(3,1,'double')
@@ -169,11 +209,9 @@ else
     tBSeuCallUII=NaN(numel(Methods),1); rBSeuCallUII=NaN(numel(Methods),1);
 end
 
-cd(rootpath);
-
 %% Problem 1 b) II
 
-if full_test
+if p2b || run_all
     display('Problem 1 b) II');
     rootpath=pwd;
     S=[97,98,99]; K=100; T=0.25; r=0.1; sig=0.01;
@@ -198,6 +236,8 @@ if full_test
             end
         end
     end
+    
+    cd(rootpath);
 else
     timeBSamPutUII = NaN(3,1,'double')
     relerrBSamPutUII = NaN(3,1,'double')
@@ -205,11 +245,9 @@ else
     tBSamPutUII=NaN(numel(Methods),1); rBSamPutUII=NaN(numel(Methods),1);
 end
 
-cd(rootpath);
-
 %% Problem 1 c) II
 
-if full_test
+if p2c || run_all
     display('Problem 1 c) II');
     rootpath=pwd;
     S=[97,98,99]; sig=0.01; r=0.1; T=0.25; K=100; B=1.25*K;
@@ -234,14 +272,14 @@ if full_test
             end
         end
     end
+    
+    cd(rootpath);
 else
     timeBSupoutCallII = NaN(3,1,'double')
     relerrBSupoutCallII = NaN(3,1,'double')
 
     tBSupoutCallII=NaN(numel(Methods),1); rBSupoutCallII=NaN(numel(Methods),1);
 end
-
-cd(rootpath);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if ispc %windows
