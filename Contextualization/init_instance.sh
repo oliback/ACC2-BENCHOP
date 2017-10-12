7004a6523f649410ae3817bc907c608c34e232d0
Haubir <jagheterhaubir@gmail.com>
2017-10-12 13:22:47 +0000

    Instance is successfully initiated

---
+++ Contextualization/init_instance.sh
@@ -0,0 +1,3 @@
+echo "Creating and initiating instance...."
+
+nova boot --flavor 8e704ef9-74dc-495e-9e2b-baebc6775b16 --image 9fea0ce0-e500-4ed3-af74-7da48d24895e --user-data instance.yml --nic net-id=7d1c469d-48ce-463d-8173-f3efbf8cb900 test
