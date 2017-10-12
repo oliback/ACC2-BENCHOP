9bab48c5a8f81946d888291daae1744dd9e1a50a
okar0286 <okan-arabaci@hotmail.com>
2017-10-12 16:44:41 +0000

    simple version

--- oct2.py
+++ oct2.py
@@ -1,7 +1,6 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 import subprocess
-#import sys
 
 
 app = Flask(__name__)
@@ -10,16 +9,25 @@ octave = "octave"
 tableM = "Table.m"
 
 
-@app.route('/test', methods=['GET'])
-def cow_say():
-	testdata = "test"
-	subprocess.call("date")
-	return testdata
+#run all problems
+@app.route('/runall', methods=['GET'])
+def run_all():
+	return str(subprocess.call([octave, tableM, "all"]))
 
+
+#run p1a
 @app.route('/p1a', methods=['GET'])
 def p1a():
-	return str(subprocess.call([octave, tableM, "p1a"]))
-	 
+	return str(subprocess.call([octave, tableM, "p1a"])) 
+
+
+#input args
+@app.route('/recompute', methods=['GET'])
+def input():
+
+	#not finished
+	return str(subprocess.call([octave, tableM, problem, parameter]))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
